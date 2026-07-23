3115b5b435db9662085a4702d9f292aed413809c
RadheyPrajapati <radheym2006@gmail.com>
2026-07-23 13:08:38 +0530

    refactor(frontend): update API calls to use deployed backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create configured Axios instance pointing to backend REST API
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://incubyte-car-dealership-assignment.onrender.com/api'
 });
 
 // Request Interceptor: Automatically attach Bearer token from localStorage
